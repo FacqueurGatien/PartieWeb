4c1876837d50e6207202b5ff7bf509fdd9f89848
Facqueur <104431014+FacqueurGatien@users.noreply.github.com>
2023-06-19 09:22:47 +0200

    update employee

---
+++ Employee copy/js/Employee.js
@@ -0,0 +1,42 @@
+class Employee{
+    constructor(_employee){
+        this.id = _employee.id;
+        this.fullName = _employee.employee_name;
+        this.salary = _employee.employee_salary;
+        this.year = _employee.employee_age;
+    }
+    CopieEmp(_id){
+        return [_id,this.fullName,this.GetEmail(),this.GetSalaryMonth(),this.GetYearBirth()]
+    }
+    GetValues(){
+        return [this.id,this.fullName,this.GetEmail(),this.GetSalaryMonth(),this.GetYearBirth()];
+    }
+
+    GetKeys(){
+
+        return ["Id","FullName","Email","Salary Month","Year of Birth","Actions"];
+    }
+
+    GetNameArray(){
+        return [this.fullName.split(' ')[0],this.fullName.split(' ')[1]]
+    }
+
+    GetEmail(){
+        let name = this.GetNameArray();
+        return name[0][0].toLowerCase()+"."+name[1].toLowerCase()+"@email.com";
+    }
+
+    GetSalaryMonth(){
+        return (this.salary/12).toFixed(2);
+    }
+
+    GetYearBirth(){
+        let date = new Date();
+        return date.getFullYear()-this.year;
+    }
+
+    GetEmployeeFormat(){
+        return [this.id,this.fullName,this.GetEmail(),this.GetSalaryMonth(),this.GetYearBirth()];
+    }
+}
+export {Employee};
